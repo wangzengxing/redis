3403e12fba514196d57fa348d2b00de75d47b258
desktop <desktop@DESKTOP-C4A2Q94>
2020-09-12 14:29:53 +0800

    分布式缓存示例

---
+++ src/Redis.Demo/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Redis.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            var buffer = Encoding.UTF8.GetBytes("Hello");
+            _distributedCache.Set("test", buffer, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10)
+            });
+        }
+    }
+}
